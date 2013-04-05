8a1d4091aa74426fd197420f4909e571bcc5b704
Peter Ren <lcdcdr2004@gmail.com>
2013-04-06 01:04:05 +0800

    Added missing files that forgot to commit for feed display logic.

--- app.rb
+++ app.rb
@@ -2,6 +2,8 @@ require 'sinatra'
 require 'sinatra/activerecord'
 require 'sinatra/reloader' if (development? && !defined? $_rakefile)
 require 'haml'
+require 'rss'
+require 'open-uri'
 
 require './models/Feed'
 
@@ -15,4 +17,26 @@ get '/feeds/list' do
 	@feeds = Feed.order("created_at DESC")
 	@feed = Feed.new
 	haml :"feeds/list"
+end
+
+get '/feeds/show/:id' do |id|
+	feed = Feed.find_by_id(id)
+	redirect not_found if !feed
+
+	rss_content = ""
+	begin
+		open(feed.url) do |f|
+			rss_content = f.read
+		end
+	rescue Exception => e
+		return "RSS source is broken... Please check your RSS source and see if it's still available."
+	end
+
+	@rss = RSS::Parser.parse(rss_content, false)
+
+	haml :"feeds/show"
+end
+
+not_found do
+	"This is not found~~"
 end
